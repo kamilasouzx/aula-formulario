86a7ddc611f91e923acc8b3ab481ec7f636b4d38
ProfHorizonteJunior <horizonte.junior@edu.sc.senai.br>
2025-09-29 15:02:38 -0300

    Adicionado o primeiro formulario

--- aula-form/src/App.js
+++ aula-form/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {useForm} from 'react-hook-form';
 
 function App() {
+  const {register, 
+        handleSubmit, 
+        formState: {errors}, 
+        reset } = useForm({mode: 'onBlur'});
+        //Arrow Function
+  const onSubmit = (data) => {
+    console.log("Dados enviados com sucesso", data)
+    reset();
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Criando Formulários</h1>
+    <form onSubmit={handleSubmit(onSubmit)}  noValidate>
+      <label>Nome </label>
+    <input placeholder='Digite seu nome'
+    {...register("nome", {required: "O nome é obrigatório"})}
+    />
+    {errors.nome && <p style={{color: "red"}}>{errors.nome.message}</p>}
+    <br/>
+    <label>Email </label>
+    <input placeholder='Digite seu email' type='email'
+    {...register("email", {required: "O email é obrigatório"})}
+    />
+        {errors.email && <p style={{color: "red"}}>{errors.email.message}</p>}<br/> 
+    <label>Senha </label>
+    <input placeholder='Digite sua senha' type='password'
+    {...register("senha", {required: "A senha é obrigatória"})}
+    /><br/>
+        {errors.senha && <p style={{color: "red"}}>{errors.senha.message}</p>}
+    <button type='submit'>Enviar</button>
+    </form>
+    </div>  
   );
 }
 
