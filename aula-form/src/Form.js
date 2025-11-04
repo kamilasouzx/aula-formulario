0ebcce09dc9c9a8904b5a8548b662b09970cb1ef
kamila_souza-silva <kskamila39@gmail.com>
2025-11-04 16:56:30 -0300

    Refactor Form component to improve error handling and streamline submission logic

--- aula-form/src/Form.js
+++ aula-form/src/Form.js
@@ -1,5 +1,5 @@
 import { useForm } from 'react-hook-form';
-import React, { useState, UseEffect } from "react";
+import React, { useState } from "react";
 
 export function Form() {
 
@@ -10,78 +10,76 @@ export function Form() {
     formState: { errors },
     reset } = useForm({ mode: 'onBlur' });
   //Arrow Function
-  const onSubmit = async () => {
+  const onSubmit = async (data) => {
 
     try {
       const response = await fetch('http://localhost:8080/api/cadastro',
         {
-          method: "POST",
+          method: 'POST',
           headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/json'
           },
           body: JSON.stringify(data),
         }
       );
-      if (!response.ok) {
+      if (response.ok) {
         const mensagemSucesso = await response.text();
-        setMensagem({mensagemSucesso });
-
-        console.log("Resposta do backend:", mensagemSucesso);
-        reset(); 
+        setMensagem({ mensagemSucesso })
+        console.log("Resposta do backend", mensagemSucesso);
+        reset()
       } else {
         const mensagemFalhou = await response.text();
-        console.error("Erro ao cadastrar")
+        setMensagem({ mensagemFalhou })
+        console.error('Erro ao cadastrar.');
 
-        const data = await response.json(); //converte a resposta para json
-        setMensagem(data.mensagem); //atualiza o estado com a mensagem recebida
-      } 
-    }catch (error) {
-        console.error('Erro ao buscar a mensagem:', error);
-        setMensagem("Erro ao conectar no backend, verifique a conexão.");
       }
-    };
+    } catch (error) {
+      console.error('Erro ao buscar a mensagem:', error);
+      setMensagem('Erro ao buscar a mensagem no servidor.');
 
-    return (
-      <div>
-        <h1 className='text-3xl text-900 mb-4'>Criando Formulários</h1>
-        <form className='space-y-2' onSubmit={handleSubmit(onSubmit)} noValidate>
-          <label className='block text-sm font-medium text-gray-700'>Nome </label>
-          <input
-            placeholder='Digite seu nome'
-            {...register("nome", { required: "O nome é obrigatório" })}
-            className="w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-          />
-          {errors.nome && <p style={{ color: "red" }}>{errors.nome.message}</p>}
-          <br />
-          <label className='block text-sm font-medium text-gray-700'>Email </label>
-          <input placeholder='Digite seu email' type='email'
-            {...register("email", { required: "O email é obrigatório", pattern: { value: /^[^\s@]+@[^\s]+\.[^\s]+$/, message: "Email inválido" } })}
-            className="w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-          />
-          {errors.email && <p style={{ color: "red" }}>{errors.email.message}</p>}<br />
-          <label className='block text-sm font-medium text-gray-700'>Senha </label>
-          <input placeholder='Digite sua senha' type='password'
-            {...register("senha", { required: "A senha é obrigatória", minLength: { value: 6, message: "A senha deve ter no mínimo 6 caracteres" } })}
-            className="w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-          /><br />
-          {errors.senha && <p style={{ color: "red" }}>{errors.senha.message}</p>}
-          <label className='block text-sm font-medium text-gray-700'>Telefone </label>
-          <input placeholder='(XX)99999999' type='tel' //criar uma máscara, ajustar o placeholder
-            {...register("telefone", { required: "O telefone é obrigatório", pattern: { value: /^\(\d{2}\)\d{9}$/, message: "Telefone inválido" } })}
-            className="w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-          /><br />
-          {errors.telefone && <p style={{ color: "red" }}>{errors.telefone.message}</p>}
-          <label >Data de Nascimento </label>
-          <input type='date'
-            {...register("dataNascimento", { required: "A data de nascimento é obrigatória" })}
-            className="w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-          /><br />
-          {errors.dataNascimento && <p style={{ color: "red" }}>{errors.dataNascimento.message}</p>}
-          <div className='flex justify-center'>
-            <button className="w-1/2 mt-2 p-3 text-white rounded-lg bg-sky-500 hover:bg-sky-700" type='submit'>Enviar</button>
-          </div>
-        </form>
-        {mensagem}
-      </div>
-    )
+    }
   }
+
+  return (
+    <div>
+      <h1 className='text-3xl text-900 mb-4'>Criando Formulários</h1>
+      <form className='space-y-2' onSubmit={handleSubmit(onSubmit)} noValidate>
+        <label className='block text-sm font-medium text-gray-700'>Nome </label>
+        <input
+          placeholder='Digite seu nome'
+          {...register("nome", { required: "O nome é obrigatório" })}
+          className="w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+        />
+        {errors.nome && <p style={{ color: "red" }}>{errors.nome.message}</p>}
+        <br />
+        <label className='block text-sm font-medium text-gray-700'>Email </label>
+        <input placeholder='Digite seu email' type='email'
+          {...register("email", { required: "O email é obrigatório", pattern: { value: /^[^\s@]+@[^\s]+\.[^\s]+$/, message: "Email inválido" } })}
+          className="w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+        />
+        {errors.email && <p style={{ color: "red" }}>{errors.email.message}</p>}<br />
+        <label className='block text-sm font-medium text-gray-700'>Senha </label>
+        <input placeholder='Digite sua senha' type='password'
+          {...register("senha", { required: "A senha é obrigatória", minLength: { value: 6, message: "A senha deve ter no mínimo 6 caracteres" } })}
+          className="w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+        /><br />
+        {errors.senha && <p style={{ color: "red" }}>{errors.senha.message}</p>}
+        <label className='block text-sm font-medium text-gray-700'>Telefone </label>
+        <input placeholder='(XX)99999999' type='tel' //criar uma máscara, ajustar o placeholder
+          {...register("telefone", { required: "O telefone é obrigatório", pattern: { value: /^\(\d{2}\)\d{9}$/, message: "Telefone inválido" } })}
+          className="w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+        /><br />
+        {errors.telefone && <p style={{ color: "red" }}>{errors.telefone.message}</p>}
+        <label >Data de Nascimento </label>
+        <input type='date'
+          {...register("dataNascimento", { required: "A data de nascimento é obrigatória" })}
+          className="w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+        /><br />
+        {errors.dataNascimento && <p style={{ color: "red" }}>{errors.dataNascimento.message}</p>}
+        <div className='flex justify-center'>
+          <button className="w-1/2 mt-2 p-3 text-white rounded-lg bg-sky-500 hover:bg-sky-700" type='submit'>Enviar</button>
+        </div>
+      </form>
+    </div>
+  )
+}
